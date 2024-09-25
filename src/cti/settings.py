3b5dc97e15fcf2272cf12ae973bfc35812abc107
Mateus Herrera <44376582+mateusherrera@users.noreply.github.com>
2024-09-25 13:42:44 -0300

    fix: ajustando autenticação para ambiente de testes

--- src/cti/settings.py
+++ src/cti/settings.py
@@ -168,36 +168,63 @@ STATIC_ROOT = path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = 'media/'
 MEDIA_ROOT = path.join(BASE_DIR, 'media')
 
-# DRF
-REST_FRAMEWORK = {
-    # Authentication
-    'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
-    ),
+# Django Rest Framework
+if DEBUG:
+    REST_FRAMEWORK = {
+        # Authentication
+        # 'DEFAULT_AUTHENTICATION_CLASSES': (
+        #     'rest_framework_simplejwt.authentication.JWTAuthentication',
+        # ),
 
-    # Permissions
-    'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.DjangoModelPermissions',
-    ),
+        # Permissions
+        'DEFAULT_PERMISSION_CLASSES': (
+            'rest_framework.permissions.DjangoModelPermissions',
+        ),
 
-    # Pagination
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 30,
+        # Pagination
+        'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+        'PAGE_SIZE': 30,
 
-    # Throttling
-    'DEFAULT_THROTTLE_CLASSES': (
-        'rest_framework.throttling.UserRateThrottle',
-    ),
-    'DEFAULT_THROTTLE_RATES': {
-        'user': '100/minute',
-    },
-}
+        # Throttling
+        'DEFAULT_THROTTLE_CLASSES': (
+            'rest_framework.throttling.UserRateThrottle',
+        ),
+        'DEFAULT_THROTTLE_RATES': {
+            'user': '100/minute',
+        },
+    }
 
-SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(seconds=60),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
-    'ROTATE_REFRESH_TOKENS': False,
-}
+else:
+    REST_FRAMEWORK = {
+        # Authentication
+        'DEFAULT_AUTHENTICATION_CLASSES': (
+            'rest_framework_simplejwt.authentication.JWTAuthentication',
+        ),
+
+        # Permissions
+        'DEFAULT_PERMISSION_CLASSES': (
+            'rest_framework.permissions.DjangoModelPermissions',
+        ),
+
+        # Pagination
+        'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+        'PAGE_SIZE': 30,
+
+        # Throttling
+        'DEFAULT_THROTTLE_CLASSES': (
+            'rest_framework.throttling.UserRateThrottle',
+        ),
+        'DEFAULT_THROTTLE_RATES': {
+            'user': '100/minute',
+        },
+    }
+
+    # Simple JWT
+    SIMPLE_JWT = {
+        'ACCESS_TOKEN_LIFETIME': timedelta(seconds=60),
+        'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
+        'ROTATE_REFRESH_TOKENS': False,
+    }
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
