30603495a4f2c385d2b2ff8e99693c3440cb322b
Mateus <mateusherreragb05@gmail.com>
2025-02-09 20:39:38 -0300

    FEAT: Adicionando ViewSet no urld.py

--- api/alert_param/urls.py
+++ api/alert_param/urls.py
@@ -12,18 +12,21 @@ from alert_param.views import (
     ForumViewSet,
     EmailViewSet,
     KeywordViewSet,
+    PostAlertedViewSet,
 )
 
 
 # Recursos
-ALERTS      = 'alert'
-FORUMS      = 'forum'
-EMAILS      = 'email'
-KEYWORDS    = 'keyword'
+ALERTS       = 'alert'
+FORUMS       = 'forum'
+EMAILS       = 'email'
+KEYWORDS     = 'keyword'
+POST_ALERTED = 'post_alerted'
 
 # Registra as views no roteador
 alert_param_router = SimpleRouter()
-alert_param_router.register( ALERTS, AlertViewSet       )
-alert_param_router.register( FORUMS, ForumViewSet       )
-alert_param_router.register( EMAILS, EmailViewSet       )
-alert_param_router.register( KEYWORDS, KeywordViewSet   )
+alert_param_router.register( ALERTS         , AlertViewSet               )
+alert_param_router.register( FORUMS         , ForumViewSet               )
+alert_param_router.register( EMAILS         , EmailViewSet               )
+alert_param_router.register( KEYWORDS       , KeywordViewSet             )
+alert_param_router.register( POST_ALERTED   , PostAlertedViewSet         )
